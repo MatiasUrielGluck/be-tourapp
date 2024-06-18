fd3c56411d0c98a1b279d1c01a12ede81fa9bced
matiasgluck <matiasugluck@gmail.com>
2024-06-17 22:00:21 -0300

    dt: eliminar metodo notificar de los estados del viaje

--- src/main/java/com/uade/be_tourapp/state/EstadoViaje/impl/EstadoCancelado.java
+++ src/main/java/com/uade/be_tourapp/state/EstadoViaje/impl/EstadoCancelado.java
@@ -1,6 +1,5 @@
 package com.uade.be_tourapp.state.EstadoViaje.impl;
 
-import com.uade.be_tourapp.entity.Viaje;
 import com.uade.be_tourapp.enums.EstadosViajeEnum;
 import com.uade.be_tourapp.state.EstadoViaje.EstadoViaje;
 import org.springframework.stereotype.Component;
